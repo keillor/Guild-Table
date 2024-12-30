51e93ec57a4da0e4f491b10904fc135c7d30add1
Keillor Jennings <69380836+keillor@users.noreply.github.com>
2024-12-29 17:46:58 -0800

    homescreen edits.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script>
+    import { Button } from "$lib/components/ui/button/index";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<Button href='/character'>Character Manage</Button>
