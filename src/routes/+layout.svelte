1a2ea36d899d613f7054d95d13d1760b1e83ebd0
Keillor Jennings <69380836+keillor@users.noreply.github.com>
2024-12-29 17:50:55 -0800

    Installed shadcn sidebar #8 into root svelte layout.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,12 @@
 <script>
 	import '../app.css';
+	import * as Sidebar from '$lib/components/ui/sidebar/index';
+	import AppSidebar from '@/components/app-sidebar.svelte';
 	let { children } = $props();
 </script>
-
-{@render children()}
+<Sidebar.Provider>
+	<AppSidebar/>
+	<Sidebar.Inset>
+		{@render children()}
+	</Sidebar.Inset>
+</Sidebar.Provider>
