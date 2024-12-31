fb32e6676c1b538d142e37a8903680a7c9501961
Keillor Jennings <69380836+keillor@users.noreply.github.com>
2024-12-30 23:38:36 -0800

    view all characters page.

--- src/routes/character/+page.svelte
+++ src/routes/character/+page.svelte
@@ -1,102 +1,130 @@
 <script>
-    import { Button } from "$lib/components/ui/button/index";
-    import * as Card from "$lib/components/ui/card/index";
-    import * as Avatar from "$lib/components/ui/avatar/index"
-    import * as AlertDialog from "$lib/components/ui/alert-dialog/index.js";
-    import { Pencil, Trash} from "lucide-svelte/icons";
-    const characters = [
-        {
-            name: 'Percy Jackson',
-            race: 'Elf',
-            class: 'Mage',
-            avatar: 'https://i.pinimg.com/736x/7f/f0/d8/7ff0d8a948987b7660a09fea4e7de228.jpg',
-            campaign: 'Toads'
-        },
-        {
-            name: 'Harry Potter',
-            race: 'Human',
-            class: 'Ranger',
-            avatar: 'https://assets-prd.ignimgs.com/2021/01/26/harry-potter-button-1611619333944.jpg',
-            campaign: 'Toads'
-        },
-        {
-            name: 'Hercules',
-            race: 'Dragonborn',
-            class: 'Rogue',
-            avatar: 'https://s.yimg.com/ny/api/res/1.2/IvwOWc7HAW93981UTEeEYw--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTQ1Mw--/https://media.zenfs.com/en/huffpost_uk_744/c3f7711ac372f2e3a47008733738a6ba',
-            campaign: 'Toads'
-        },
-        {
-            name: 'Spiderman',
-            race: 'Spider',
-            class: 'Bard',
-            avatar: 'https://parade.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTkwNTc4NzQ2NjU0ODYxMTgw/tom-holland-spider-man-ftr.jpg',
-            campaign: 'Toads'
-        }
-    ]
-</script>	
+	import { Button } from '$lib/components/ui/button/index';
+	import * as Card from '$lib/components/ui/card/index';
+	import * as Avatar from '$lib/components/ui/avatar/index';
+	import * as AlertDialog from '$lib/components/ui/alert-dialog/index.js';
+	import { Pencil, Trash, Shield, Heart, Eye } from 'lucide-svelte/icons';
+	const characters = [
+		{
+			name: 'Percy Jackson',
+			race: 'Elf',
+			class: 'Mage',
+			level: 3,
+			avatar: 'https://i.pinimg.com/736x/7f/f0/d8/7ff0d8a948987b7660a09fea4e7de228.jpg',
+			campaign: 'Toads'
+		},
+		{
+			name: 'Harry Potter',
+			race: 'Human',
+			class: 'Ranger',
+			level: 3,
+			avatar: 'https://assets-prd.ignimgs.com/2021/01/26/harry-potter-button-1611619333944.jpg',
+			campaign: 'Toads'
+		},
+		{
+			name: 'Hercules',
+			race: 'Dragonborn',
+			class: 'Rogue',
+			level: 3,
+			avatar:
+				'https://s.yimg.com/ny/api/res/1.2/IvwOWc7HAW93981UTEeEYw--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTQ1Mw--/https://media.zenfs.com/en/huffpost_uk_744/c3f7711ac372f2e3a47008733738a6ba',
+			campaign: 'Toads'
+		},
+		{
+			name: 'Spiderman',
+			race: 'Spider',
+			class: 'Bard',
+			level: 3,
+			avatar:
+				'https://parade.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTkwNTc4NzQ2NjU0ODYxMTgw/tom-holland-spider-man-ftr.jpg',
+			campaign: 'Toads'
+		}
+	];
+	const list = [
+		{ attribute: 'Strength', score: 14, modifier: 2 },
+		{ attribute: 'Consitution', score: 14, modifier: 2 },
+		{ attribute: 'Dexterity', score: 14, modifier: 2 },
+		{ attribute: 'Intelligence', score: 14, modifier: 2 },
+		{ attribute: 'Wisdom', score: 14, modifier: 2 },
+		{ attribute: 'Charisma', score: 14, modifier: 2 }
+	];
+</script>
 
-<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-    Character Manage
-</h1>
+<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Character Manage</h1>
 
-
-<Card.Root class='p-3'>
-    <Button href='/character/create'>Create New Character</Button>
+<Card.Root class="p-3">
+	<Button href="/character/create">Create New Character</Button>
 </Card.Root>
 
-<h2 class='mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0'>
-    Your Characters
+<h2
+	class="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
+>
+	Your Characters
 </h2>
-<div class='grid grid-cols-3 gap-3'>
-    {#each characters as character}
-        <Card.Root>
-            <Card.Header>
-                <div class="flex flex-row justify-between items-center">
-                    <h3 class="text-xl font-semibold">{character.name}</h3>
-                    <Avatar.Root class='h-12 w-12'>
-                        <Avatar.Image src={character.avatar}/>
-                        <Avatar.Fallback>N/A</Avatar.Fallback>
-                    </Avatar.Root>
-                </div>
-            </Card.Header>
-            <Card.Content>
-                <div>
-                    <p>Race: {character.race}</p>
-                    <p>Class: {character.class}</p>
-                    <p>Campaign: {character.campaign}</p>
-                </div>
-            </Card.Content>
-            <Card.Footer class='flex justify-between'>
-                <Button variant='outline'>
-                    <Pencil class='mr-2 size-4'/>
-                    Edit
-                </Button>
-                
-                <AlertDialog.Root>
-                    <AlertDialog.Trigger>
-                        <Button variant='outline'>
-                            <Trash class='mr-2 size-4'/>
-                            Trash
-                        </Button>
-                    </AlertDialog.Trigger>
-                    <AlertDialog.Content>
-                      <AlertDialog.Header>
-                        <AlertDialog.Title>Are you absolutely sure?</AlertDialog.Title>
-                        <AlertDialog.Description>
-                          This action cannot be undone. This will permanently delete your account
-                          and remove your data from our servers.
-                        </AlertDialog.Description>
-                      </AlertDialog.Header>
-                      <AlertDialog.Footer>
-                        <AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
-                        <AlertDialog.Action class='bg-red-300 hover:bg-red-500'>Delete</AlertDialog.Action>
-                      </AlertDialog.Footer>
-                    </AlertDialog.Content>
-                  </AlertDialog.Root>
-            </Card.Footer>
-            
-            
-        </Card.Root>
-    {/each}
+<div class="grid md:grid-cols-3 md:gap-3">
+	{#each characters as character}
+		<Card.Root>
+			<Card.Header>
+				<div class="flex flex-row items-center justify-between">
+					<h3 class="text-xl font-semibold">{character.name}</h3>
+					<Avatar.Root class="h-12 w-12">
+						<Avatar.Image src={character.avatar} />
+						<Avatar.Fallback>N/A</Avatar.Fallback>
+					</Avatar.Root>
+				</div>
+			</Card.Header>
+			<Card.Content>
+				<div class="flex justify-between">
+					<div class="flex flex-col">
+						<p>Race: {character.race}</p>
+						<p>Class: {character.class}</p>
+						<p>Level: {character.level}</p>
+						<p>Campaign: {character.campaign}</p>
+					</div>
+					<div class="grid grid-cols-3 gap-5">
+						<!-- <Shield class='size-12 border-2 rounded-md'/> -->
+						{#each list as attribute, i}
+							<div class="h-24 w-24 p-1 border-2 flex flex-col justify-between content-center text-center rounded-lg">
+                                <p class='italic'>{attribute.attribute}</p>
+                                <p class='text-xl font-bold'>{attribute.score}</p>
+                                <p class="border-2 h-min rounded-full px-2">{attribute.modifier}</p>
+							</div>
+						{/each}
+					</div>
+				</div>
+			</Card.Content>
+			<Card.Footer class="flex justify-between">
+				<Button variant="outline">
+					<Eye class="mr-2 size-4" />
+					View
+				</Button>
+				<Button variant="outline">
+					<Pencil class="mr-2 size-4" />
+					Edit
+				</Button>
+
+				<AlertDialog.Root>
+					<AlertDialog.Trigger>
+						<Button variant="outline">
+							<Trash class="mr-2 size-4" />
+							Delete
+						</Button>
+					</AlertDialog.Trigger>
+					<AlertDialog.Content>
+						<AlertDialog.Header>
+							<AlertDialog.Title>Are you absolutely sure?</AlertDialog.Title>
+							<AlertDialog.Description>
+								This action cannot be undone. This will permanently delete your account and remove
+								your data from our servers.
+							</AlertDialog.Description>
+						</AlertDialog.Header>
+						<AlertDialog.Footer>
+							<AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+							<AlertDialog.Action class="bg-red-300 hover:bg-red-500">Delete</AlertDialog.Action>
+						</AlertDialog.Footer>
+					</AlertDialog.Content>
+				</AlertDialog.Root>
+			</Card.Footer>
+		</Card.Root>
+	{/each}
 </div>
