0d830d2bede5b75a7c05f92cd0f499a3a855d038
keillor <keillorjennings123@gmail.com>
2025-01-08 22:13:11 -0800

    updated login page formatting.

--- src/routes/(login)/login/+page.svelte
+++ src/routes/(login)/login/+page.svelte
@@ -2,16 +2,26 @@
   import { Button } from "$lib/components/ui/button/index";
   import * as Card from '$lib/components/ui/card/index';
   import Input from "@/components/ui/input/input.svelte";
+	import Label from "@/components/ui/label/label.svelte";
 </script>
 
-<Card.Root>
-  <Card.Header>
-    <Card.Title>Welcome to Guild Table!</Card.Title>
-  </Card.Header>
-  <Card.Content>
-    <Input type='text' name='email' placeholder='user@email.com' class='max-w-xs'/>
-    <Input type='password' name='password' placeholder='Password' class='max-w-xs'/>
-    <Button href='/'>Login</Button>
-    <Button href='/signup'>Signup</Button>
-  </Card.Content>
-</Card.Root>
+<div class='flex h-screen w-full items-center justify-center px-4'>
+  <Card.Root class='mx-auto max-w-sm'>
+    <Card.Header>
+      <Card.Title>Welcome to Guild Table!</Card.Title>
+    </Card.Header>
+    <Card.Content class="grid gap-4">
+      <Label for='email'>Email</Label>
+      <Input type='text' name='email' placeholder='email@example.com' class='max-w-xs'/>
+
+      <Label for='password'>Password</Label>
+      <Input type='password' name='password' placeholder='Password' class='max-w-xs'/>
+
+      <Button href='/'>Login</Button>
+      <Button variant="outline" class="w-full">Login with Google</Button>
+
+      <p>Don't have an account? <a href="/signup" class="underline"> Sign up </a></p>     
+    </Card.Content>
+  </Card.Root>
+</div>
+
