22c1ab8389b828ca119a94f08b6a9cfaefb3d16b
Keillor Jennings <69380836+keillor@users.noreply.github.com>
2025-01-17 21:46:23 -0800

    changed dnd5api url

--- src/lib/api/dnd5api.js
+++ src/lib/api/dnd5api.js
@@ -1,13 +1,15 @@
 //import { DND5APIURL } from "$env/static/private";
 
+const APIURL = 'https://www.dnd5eapi.co'
+
 export async function dnd5ApiRequest(uri) {
-	const fetchResult = await fetch(`https://www.dnd5eapi.co/api/${uri}`);
+	const fetchResult = await fetch(`${APIURL}/api/${uri}`);
 	const results = await fetchResult.json();
 	return results;
 }
 
 export async function dnd5ApiRaw(uri) {
-	const fetchResult = await fetch(`https://www.dnd5eapi.com${uri}`);
+	const fetchResult = await fetch(`${APIURL}${uri}`);
 	const results = await fetchResult.json();
 	return results;
 }
