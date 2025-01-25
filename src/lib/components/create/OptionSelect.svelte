43ad180028e3253f3da84b1ba0c6795404939c96
Keillor Jennings <keillorjennings123@gmail.com>
2025-01-25 10:11:10 -0800

    added optionSelect

---
+++ src/lib/components/create/OptionSelect.svelte
@@ -0,0 +1,11 @@
+<script>
+    const {options} = $props();
+    import { Checkbox } from "$lib/components/ui/checkbox/index.js";
+    import Label from "@/components/ui/label/label.svelte";
+</script>
+
+
+{#each options as option (option.index)}
+    <Checkbox name={option.index}/>
+    <Label 
+{/each}
