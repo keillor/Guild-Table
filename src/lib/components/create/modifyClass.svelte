917c2ef80461fa7da43b6aaaf19bd91e89a922e1
keillor <69380836+keillor@users.noreply.github.com>
2025-01-25 16:47:01 -0800

    created modify class

---
+++ src/lib/components/create/modifyClass.svelte
@@ -0,0 +1,28 @@
+<script>
+	import ClassOptionSelect from "./ClassOptionSelect.svelte";
+    import * as Card from '$lib/components/ui/card/index';
+
+    const {classData, levelData, form} = $props();
+
+    let options = []
+    for (let key in classData) {
+        if(key.endsWith('choices')) {
+            options.push({data: classData[key], displayValue: key.replace(/_/g, ' ').replace(/\b\w/g, char => char.toUpperCase()), key: key});
+        }
+    }
+</script>
+
+{#if options.length > 0}
+    <Card.Root>
+        <Card.Header>
+            <h2 class='text-xl font-bold'>Class Options</h2>
+        </Card.Header>
+        <Card.Content>
+            {#each options as thing (thing.key)}
+                {#each thing.data as option (option.desc)}
+                    <ClassOptionSelect formInputName={option.desc} formDisplayName={option.desc} form={form} choices={option}/> 
+                {/each}
+            {/each}
+        </Card.Content>
+    </Card.Root>
+{/if}
