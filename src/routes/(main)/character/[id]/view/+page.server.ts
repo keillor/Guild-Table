5bc9eecdcfd85f66313967f741351daab3ab3148
keillor <69380836+keillor@users.noreply.github.com>
2025-03-19 16:43:37 -0700

    added redirect users visits a character that they don't own.

--- src/routes/(main)/character/[id]/view/+page.server.ts
+++ src/routes/(main)/character/[id]/view/+page.server.ts
@@ -1,8 +1,10 @@
 import { getSingleCharacterVerified } from '$lib/api/mongoapi_server.js'
+import { redirect } from '@sveltejs/kit';
 
 export const load = async ({locals: {session}, params}) => {
     const character = await getSingleCharacterVerified(session, params.id);
     if(character) {
         return { character };
-    } 
+    }
+    redirect(303, '/character');
 }
