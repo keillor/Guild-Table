6f1ca9e44173096b3922d4ed4fa23096f7c176f8
keillor <69380836+keillor@users.noreply.github.com>
2025-03-29 13:44:38 -0700

    fixed error.svelte

--- src/routes/(main)/+error.svelte
+++ src/routes/(main)/+error.svelte
@@ -2,7 +2,6 @@
 	import { page } from '$app/state';
 </script>
 
-<h1>{page.error.message}</h1>
 
-<h1>Whoops!</h1>
-<p>Please try again later.</p>
+<h1 class='text-3xl font-bold' >Whoops!</h1>
+<h1 class='text-3xl' >{page.error.message}</h1>
