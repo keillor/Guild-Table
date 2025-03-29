e310acdae7ac718da5e80f48ab71a02e09e64cd1
keillor <69380836+keillor@users.noreply.github.com>
2025-03-28 17:39:20 -0700

    adjusted structure to await data from server

--- src/routes/(main)/wiki/+page.svelte
+++ src/routes/(main)/wiki/+page.svelte
@@ -10,9 +10,8 @@
 
 	const { data } = $props();
 	let errorText = $state();
-	let wikiPages = $state(data.wikiPages);
+	let pageState = $state(data.wikiPages);
 </script>
-
 
 <h1 class="mb-4 text-3xl font-bold">Wiki Pages</h1>
 <AlertDialog.Root>
@@ -58,70 +57,68 @@
 {#await wikiPages}
 	<!-- //empty -->
 {:then wikiPages}
-
-{#if wikiPages.length == 0}
-	<h1 class='text-3xl'>Looks like you don't have any wiki pages yet...</h1>
-{:else}
-	<table class="w-full table-auto border-collapse border border-gray-300">
-		<thead>
-			<tr>
-				<th class="border border-gray-300 px-4 py-2">Title</th>
-				<th class="border border-gray-300 px-4 py-2">Campaign</th>
-				<th class="border border-gray-300 px-4 py-2">Actions</th>
-			</tr>
-		</thead>
-		<tbody>
-			{#each wikiPages as wiki}
+	{#if wikiPages.length == 0}
+		<h1 class="text-3xl">Looks like you don't have any wiki pages yet...</h1>
+	{:else}
+		<table class="w-full table-auto border-collapse border border-gray-300">
+			<thead>
 				<tr>
-					<td class="border border-gray-300 px-4 py-2">{wiki.title}</td>
-					<td class="border border-gray-300 px-4 py-2">{wiki.campaign || 'No Campaign'}</td>
-					<td class="flex flex-row border border-gray-300 px-4 py-2">
-						<Button
-							class={`${buttonVariants({ variant: 'outline' })} hover:underline`}
-							href={`/wiki/${wiki._id}`}
-						>
-							<Eye class="size-4 text-black" />
-						</Button>
-
-						<AlertDialog.Root>
-							<AlertDialog.Trigger class={`${buttonVariants({ variant: 'outline' })} w-min`}>
-								<Trash class="size-4" />
-							</AlertDialog.Trigger>
-							<AlertDialog.Content>
-								<AlertDialog.Header>
-									<AlertDialog.Title>Are you sure you want to delete?</AlertDialog.Title>
-									<AlertDialog.Description>This action cannot be undone!</AlertDialog.Description>
-								</AlertDialog.Header>
-								<AlertDialog.Footer>
-									<AlertDialog.Cancel type="button">Cancel</AlertDialog.Cancel>
-									<form
-										method="POST"
-										action="?/delete"
-										use:enhance={({ formData }) => {
-											formData.append('objectId', wiki._id);
-											return async ({ result }) => {
-												await result;
-												console.log(result);
-												if (result.type == 'success') {
-													//wikiPages = wikiPages.filter((w) => w._id !== wiki._id);
-												}
-											};
-										}}
-									>
-										<AlertDialog.Action
-											type="submit"
-											class={buttonVariants({ variant: 'destructive' })}
-											onclick="()">Continue</AlertDialog.Action
-										>
-									</form>
-								</AlertDialog.Footer>
-							</AlertDialog.Content>
-						</AlertDialog.Root>
-					</td>
+					<th class="border border-gray-300 px-4 py-2">Title</th>
+					<th class="border border-gray-300 px-4 py-2">Campaign</th>
+					<th class="border border-gray-300 px-4 py-2">Actions</th>
 				</tr>
-			{/each}
-		</tbody>
-	</table>
-{/if}
+			</thead>
+			<tbody>	
+				{#each pageState as wiki}
+					<tr>
+						<td class="border border-gray-300 px-4 py-2">{wiki.title}</td>
+						<td class="border border-gray-300 px-4 py-2">{wiki.campaign || 'No Campaign'}</td>
+						<td class="flex flex-row border border-gray-300 px-4 py-2">
+							<Button
+								class={`${buttonVariants({ variant: 'outline' })} hover:underline`}
+								href={`/wiki/${wiki._id}`}
+							>
+								<Eye class="size-4 text-black" />
+							</Button>
 
+							<AlertDialog.Root>
+								<AlertDialog.Trigger class={`${buttonVariants({ variant: 'outline' })} w-min`}>
+									<Trash class="size-4" />
+								</AlertDialog.Trigger>
+								<AlertDialog.Content>
+									<AlertDialog.Header>
+										<AlertDialog.Title>Are you sure you want to delete?</AlertDialog.Title>
+										<AlertDialog.Description>This action cannot be undone!</AlertDialog.Description>
+									</AlertDialog.Header>
+									<AlertDialog.Footer>
+										<AlertDialog.Cancel type="button">Cancel</AlertDialog.Cancel>
+										<form
+											method="POST"
+											action="?/delete"
+											use:enhance={({ formData }) => {
+												formData.append('objectId', wiki._id);
+												return async ({ result }) => {
+													await result;
+													console.log(result);
+													if (result.type == 'success') {
+														pageState = wikiPages.filter((w) => w._id !== wiki._id);
+													}
+												};
+											}}
+										>
+											<AlertDialog.Action
+												type="submit"
+												class={buttonVariants({ variant: 'destructive' })}
+												onclick="()">Continue</AlertDialog.Action
+											>
+										</form>
+									</AlertDialog.Footer>
+								</AlertDialog.Content>
+							</AlertDialog.Root>
+						</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+	{/if}
 {/await}
