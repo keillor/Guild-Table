1c269406d89ae4b8874365b3abf71ebf886ec17a
keillor <69380836+keillor@users.noreply.github.com>
2025-03-28 21:42:34 -0700

    added equipment count to equipment screen

--- src/routes/(main)/character/[id]/equipment/+page.svelte
+++ src/routes/(main)/character/[id]/equipment/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import Button from '$lib/components/ui/button/button.svelte';
 	import CharacterLinks from '$lib/components/view/characterLinks.svelte';
-	import { Info, Trash } from 'lucide-svelte';
+	import { Info, Minus, Plus, Trash } from 'lucide-svelte';
 	import type { Equipment } from '$lib/models/character';
 	import { defaults, superForm } from 'sveltekit-superforms';
 	import { equipmentSchema } from './schema.js';
@@ -11,6 +11,8 @@
 	import EquipmentAsyncPopover from '$lib/components/view/EquipmentAsyncPopover.svelte';
 	import { toast } from 'svelte-sonner';
 	import Label from '$lib/components/ui/label/label.svelte';
+	import Input from '$lib/components/ui/input/input.svelte';
+	import { buttonVariants } from '$lib/components/ui/button/index.js';
 
   const { data } = $props();
   const character = data.character;
@@ -19,11 +21,19 @@
   let equipment : Equipment[] = character.equipment;
 
   let totalWeight = $derived.by(() => {
-    return equipment.reduce((accumulator, currentVal) => accumulator + currentVal.weight, 0)
+    return $formData.equipment.reduce((accumulator, currentVal) => accumulator + currentVal.weight, 0)
   })
 
   const removeItem = (item: Equipment) => {
     $formData.equipment = $formData.equipment.filter((o) => o.index !== item.index);
+  }
+
+  const increment = (index: number) => {
+    $formData.equipment[index].count += 1;
+  }
+
+  const decrement = (index: number) => {
+    $formData.equipment[index].count -= 1;
   }
 
   async function addEquipment(item) {
@@ -73,13 +83,14 @@
         <tr>
           <th class="border border-gray-300 px-4 py-2">Active</th>
           <th class="border border-gray-300 px-4 py-2">Name</th>
+          <th class='border border-gray-300 px-4 py-2'>Count</th>
           <th class="border border-gray-300 px-4 py-2">Weight</th>
           <th class="border border-gray-300 px-4 py-2">Cost</th>
           <th class="border border-gray-300 px-4 py-2">Action</th>
         </tr>
       </thead>
       <tbody>
-        {#each $formData.equipment as item}
+        {#each $formData.equipment as item, index}
           <tr>
             <td class="border border-gray-300 px-4 py-2">
               <input
@@ -89,6 +100,11 @@
               />
             </td>
             <td class="border border-gray-300 px-4 py-2">{item.name}</td>
+            <td class="border border-gray-300 px-4 py-2 flex flex-row">
+              <Input class='w-min' min=0 value={$formData.equipment[index].count} />
+              <Button class={buttonVariants({variant: 'outline'})} onclick={() => decrement(index)}><Minus class='size-4 text-black'/></Button>
+              <Button class={buttonVariants({variant: 'outline'})} onclick={() => increment(index)}><Plus class='size-4 text-black'/></Button>
+            </td>
             <td class="border border-gray-300 px-4 py-2">{item.weight}</td>
             <td class="border border-gray-300 px-4 py-2">
               {item.cost.quantity} {item.cost.unit}
