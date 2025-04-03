a41f7d75b053109cfaa0480a2667d53a505b0b0a
keillor <69380836+keillor@users.noreply.github.com>
2025-04-03 12:47:14 -0700

    refactored into svelte5 syntax

--- src/routes/vtt/+page.svelte
+++ src/routes/vtt/+page.svelte
@@ -1,49 +1,59 @@
-<script>
-// @ts-nocheck
+<script lang='ts'>
 
   import * as Card from "$lib/components/ui/card/index.js";
   import * as Popover from "$lib/components/ui/popover/index.js";
   import {Button, buttonVariants} from "$lib/components/ui/button/index.js";
   import { Backpack, Dices, Heart, ScrollText, Sparkles, Swords, UserRound } from "lucide-svelte";
+	import Input from "$lib/components/ui/input/input.svelte";
 
   const items = [
     {
       title: "rolls",
-      icon: Dices
+      icon: Dices,
+      component: Input
     },
     { title: "character-attributes",
-      icon: UserRound
+      icon: UserRound,
+      component: Input
     },
     {
       title: "other-information",
-      icon: Sparkles
+      icon: Sparkles,
+      component: Input
     },
     {
       title: "saving-throws",
-      icon: Sparkles
+      icon: Sparkles,
+      component: Input
     },
     { 
       title: "health", 
-      icon: Heart
+      icon: Heart,
+      component: Input
     },
     {
       title: "weapons",
-      icon: Swords
+      icon: Swords,
+      component: Input
     },
     {
       title: "features-and-traits",
-      icon: ScrollText
+      icon: ScrollText,
+      component: Input
     },
     {
       title: "inventory",
-      icon: Backpack
+      icon: Backpack,
+      component: Input
     }
   ];
 
-  let activeItem = null; // Initialize as a regular variable
+  let ActiveItem = $state(null);
+  let gameDivRef = $state<HTMLElement>(null!);
 
   function setActiveComponent(item) {
-    activeItem = item; // Update activeItem reactively
+    console.log('item set', item);
+    ActiveItem = item.icon;
   }
 
   
@@ -53,14 +63,18 @@
   id="button-container-card" 
   class="absolute left-0 w-20 h-full m-5 p-0 rounded-lg shadow-lg">
   <Card.Content>
-    <div id="button-container" class="flex flex-col items-center space-y-5">
+    <div id="button-container" class="flex flex-col items-center">
       {#each items as item}
-        <Popover.Root>
+        <Popover.Root open={false}>
           <Popover.Trigger 
-            class="${buttonVariants({ variant: 'outline' })} flex items-center mx-0 p-2 m-1"
-            on:click={() => setActiveItem(item)}>
+            class="${buttonVariants({ variant: 'outline' })} flex items-center mx-0 p-2 m-1 aspect-square"
+            onclick={() => setActiveComponent(item)}
+            >
               <item.icon class="w-7 h-10"/>
           </Popover.Trigger>
+          <Popover.Content customAnchor={gameDivRef}>
+            <item.icon/>
+          </Popover.Content>
         </Popover.Root>
       {/each}
     </div>
@@ -75,7 +89,12 @@
   id="game-card" 
   class="absolute left-20 right-0 h-full m-5 p-0 rounded-lg shadow-lg">
   <Card.Content>
-    <svelte:component this={ActiveComponent ? activeItem.icon : null} />
+    <div bind:this={gameDivRef}>
+  <!--     {#if ActiveItem != null}
+        <ActiveItem />
+      {/if} -->
+      <p>Game</p>
+    </div>
   </Card.Content>
 </Card.Root>
 
