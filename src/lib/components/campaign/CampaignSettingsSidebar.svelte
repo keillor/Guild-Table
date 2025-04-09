b11fe3596589a7924918bc19514cd894b4d34455
keillor <69380836+keillor@users.noreply.github.com>
2025-04-09 13:24:12 -0700

    campaign layout

---
+++ src/lib/components/campaign/CampaignSettingsSidebar.svelte
@@ -0,0 +1,26 @@
+<script>
+	import Button from "../ui/button/button.svelte";
+
+    const settingsPages = [
+        {
+            name: 'Campaign',
+            url: '.'
+        },
+        {
+            name: 'Wiki',
+            url: './wiki'
+        },
+        {
+            name: 'Characters',
+            url: './characters'
+        }
+    ]
+</script>
+
+<div class='flex flex-row w-min'>
+    {#each settingsPages as link}
+        <Button variant='outline' href={link.url}>
+            {link.name}
+        </Button>
+    {/each}
+</div>
---
+++ src/routes/(main)/campaign/[id]/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import CampaignSettingsSidebar from "$lib/components/campaign/CampaignSettingsSidebar.svelte";
+
+    const { children } = $props();
+</script>
+
+
+<CampaignSettingsSidebar/>
+{@render children()}
